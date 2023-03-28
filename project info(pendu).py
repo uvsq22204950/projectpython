1315518ab5a229d029770ff0b2b7488445759747
22102661TB <128298934+22102661TB@users.noreply.github.com>
2023-03-28 07:56:03 +0200

    Update project info(pendu).py
    j'ai ajouté le bonhome

--- project info(pendu).py
+++ project info(pendu).py
@@ -33,6 +33,36 @@ def afficher_mot_cache(mot, lettres_trouvees):
             mot_cache += lettre
         else:
             mot_cache += "_"
+            # Fonction pour mettre à jour l'affichage
+def maj_affichage():
+    # Effacer le canevas et redessiner le pendu
+    canevas.delete("all")
+    if erreurs > 0:
+        canevas.create_line(20, 180, 100, 180)
+    if erreurs > 1:
+        canevas.create_line(60, 180, 60, 20)
+    if erreurs > 2:
+        canevas.create_line(60, 20, 150, 20)
+    if erreurs > 3:
+        canevas.create_line(150, 20, 150, 40)
+    if erreurs > 4:
+        canevas.create_oval(135, 40, 165, 70)
+    if erreurs > 5:
+        canevas.create_line(150, 70, 150, 110)
+    # Afficher les lettres correctes et incorrectes
+    lettres_correctes_lbl.configure(text="Lettres correctes: " + ", ".join(lettres_correctes))
+    lettres_incorrectes_lbl.configure(text="Lettres incorrectes: " + ", ".join(lettres_incorrectes))
+    # Afficher les lettres du mot cachées ou dévoilées
+    mot_lbl.configure(text="Mot: " + " ".join(l if l in lettres_correctes else "_" for l in mot))
+
+# Fonction pour afficher un message de fin de jeu
+def afficher_message(message):
+    message_lbl.configure(text=message)
+    message_lbl.pack()
+
+# Créer la fenêtre principale
+fenetre = Tk()
+fenetre.title("Jeu du pendu")
     return mot_cache
 
 
