8eef92d9c757d9476d50c3f4aefcdf5b8b8d397e
Painpas Perdu <114142519+22206088@users.noreply.github.com>
2023-04-17 23:45:30 +0200

    Update Pendu project IN200.py

--- Pendu project IN200.py
+++ Pendu project IN200.py
@@ -20,46 +20,46 @@ for i in range(len(mot_tab)):#met le nombre de lettre du mot en étoile dans la 
     affichage.append("*")
 
 def indication(): #donne une lettre qui a un haut to de fréquance dans la langue française
-    entry_letter.delete(0,tk.END)
+    entry_letter.delete(0,tk.END) # supprime l'intérieur de la case à écrire (de 0 jusqu'à la fin)
     entry_letter.insert(0,frequence[0])
 
 def aide(): #fonction aide qui nous donne les règles
     aides =tk.Tk()
     label = tk.Label(aides, text="Le but du jeu est simple : deviner toutes les lettres qui doivent composer un mot, éventuellement avec un nombre limité de tentatives et des thèmes fixés à l'avance.\n" + "A chaque fois que le joueur devine une lettre, celle-ci est affichée. Dans le cas contraire, le dessin d'un pendu se met à apparaître", font=("Helvetica", 9))
-    label.pack()
+    label.pack()  #afficher le label, sans choisir ou c'est
     aides.mainloop()
 #fonction qui s'occupe de gerer comment le jeu fonctionne le nb de lettre les proposition si c deja proposé si c gagné ou non
 def valider(event):
-    global bonnes_lettres
+    global bonnes_lettres  # variable que l'on utilise en dehors de la fonction
     global mauvaises_lettres
     global affichage
     global tentative
-    proposition=entry_letter.get()
-    entry_letter.delete(0,tk.END)
+    proposition=entry_letter.get()  # récupère ce qu'il y a dans entry letter
+    entry_letter.delete(0,tk.END)   # clear la case pour écrire
     proposition = proposition.lower()
     x = proposition.isalpha()
     if len(proposition) > 1 or x == False:
         label_error.config(text="erreur, recommencer")
 
-    if len(proposition) <= 1 and x == True:
-        if proposition in bonnes_lettres or proposition in mauvaises_lettres:
+    if len(proposition) <= 1 and x == True:  # vérifie si le mot est valide
+        if proposition in bonnes_lettres or proposition in mauvaises_lettres: #vérifie si la lettre à déjà été proposé
             label_error.config(text=proposition + " a déjà été proposé!")
             return
-        frequence.remove(proposition)
+        frequence.remove(proposition)  # enlève la proposition de la fréquence des lettres car elle a déjà été dite
 
         for i in range(len(mot_tab)):
-            if proposition == mot_tab[i]:
+            if proposition == mot_tab[i]:  # détermine si la lettre est dans le mot
                 if proposition not in bonnes_lettres:
                     bonnes_lettres += [proposition]
-                label_tab[i].config(text=str(proposition))
+                label_tab[i].config(text=str(proposition))   # configure l'astérix pour qu'il devienne la lettre déterminé à l'indice i
                 affichage.pop()
 
         if proposition not in solution :#pour les tentative et la victoire
             mauvaises_lettres += [proposition]
-            label_F.config(text="mauvaises lettres: " + str(mauvaises_lettres))
+            label_Faux.config(text="mauvaises lettres: " + str(mauvaises_lettres))
             tentative -= 1
             dessin()
-            label_T.config(text="tentatives restantes: " + str(tentative))
+            label_True.config(text="tentatives restantes: " + str(tentative))
         if tentative <= 0:
             tkinter.messagebox.showinfo("GAME OVER", "le mot est " + solution)
             root.destroy()
@@ -71,21 +71,22 @@ root.title("PENDU")
 
 label_tab = []
 for i in range(len(mot_tab)):
-    label = tk.Label(root, text="*", font=("Helvetica", 30))
+    label = tk.Label(root, text="*", font=("Helvetica", 30))   # chaque étoiles = un label
     label.grid(row=2, column=i)
     label_tab.append(label)
 
 label_error = tk.Label(root, text="")
 label_error.grid(row=3,column=97)
-label_F = tk.Label(root, text="")
-label_F.grid(row=3,column=98)
-label_T = tk.Label(root, text="nombre de tentatives restantes: " + str(tentative))
-label_T.grid(row=20,column=600)
+label_Faux = tk.Label(root, text="")  # label mauvaises lettres
+label_Faux.grid(row=3,column=98)
+label_True = tk.Label(root, text="nombre de tentatives restantes: " + str(tentative))   # label tentative
+label_True.grid(row=20,column=600)
+label_True.grid(row=20,column=600)
 
-entry_letter = tk.Entry(root, width=2, font=("Helvetica", 30), justify= CENTER)
+entry_letter = tk.Entry(root, width=2, font=("Helvetica", 30), justify= CENTER)   # texte écrit au centre de la case
 entry_letter.grid(row=20,column=700)
 #programmation des boutons de tout les boutons
-bouton_V= tk.Button(root, text="valider", bg="green", relief="groove", borderwidth=5, command=lambda : valider(any))
+bouton_V= tk.Button(root, text="valider", bg="green", relief="groove", borderwidth=5, command=lambda : valider(any))   # lambda pour ajouter des paramètres/ any paramètre on met ce qu'on veut dedans
 bouton_V.grid(row=20,column=1000)
 
 ButtonI = tk.Button(root, width=8, height=1, bg="grey", relief="groove", borderwidth=5, text="indication", command=indication)
